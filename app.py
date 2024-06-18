f613c1d7792c2cb0dd4ad46f5a40860eabea5520
dyavadi8769 <sdyavadi@tamu.edu>
2024-06-18 01:49:48 -0500

    app is in progress

--- app.py
+++ app.py
@@ -32,12 +32,36 @@ pickle_file = os.path.join(raw_local_dir_path,img_pickle_file_name)
 
 #feature path
 
-feature_extractor_dir = artifacts['feature_extraction_dir']
+feature_extraction_dir = artifacts['feature_extraction_dir']
 extracted_features_name= artifacts['extracted_features_name']
-feature_extraction_path=os.path.join(artifacts_dir, feature_extractor_dir)
-feature_name = os.path.join(feature_extraction_path, extracted_features_name)
+feature_extraction_path=os.path.join(artifacts_dir, feature_extraction_dir)
+features_name = os.path.join(feature_extraction_path, extracted_features_name)
 
 model_name = params['base']['BASE_MODEL']
 include_tops= params['base']['include_top']
 poolings= params['base']['pooling']
 
+#sace upload image
+def save_upload_image():
+    try:
+        create_directory(dirs=[upload_path])
+
+        with open(os.path.join(upload_path,uploadimage.name),'wb') as f:
+            f.write(uploadimage.getbuffer())
+
+        return True
+    except:
+        return False
+
+
+
+#streamlit
+st.title("To whom does your face match")
+
+uploadimage=st.file_uploader('Choose an Image')
+
+if uploadimage is not None:
+    #save the image
+    if save_upload_image(uploadimage):
+        #loading the image
+        display_image=Image.open(uploadimage)
